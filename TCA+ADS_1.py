aaffff7b539eb56a4ebb96c67dc703828e325684
Nicolamb160 <69460211+Nicolamb160@users.noreply.github.com>
2022-03-10 10:51:24 +0100

    Update TCA+ADS_1.py

--- TCA+ADS_1.py
+++ TCA+ADS_1.py
@@ -12,11 +12,11 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 # Create the TCA9548A object and give it the I2C bus
 tca = adafruit_tca9548a.TCA9548A(i2c, address=0X70)
 
-for channel in range(8):
-    if tca[channel].try_lock():
-        print("Channel {}:".format(channel), end="")
-        addresses = tca[channel].scan()
-        print([hex(address) for address in addresses if address != 0x70])
+#for channel in range(8):
+    #if tca[channel].try_lock():
+        #print("Channel {}:".format(channel), end="")
+        #addresses = tca[channel].scan()
+        #print([hex(address) for address in addresses if address != 0x70])
 
 # Create the ADC object using the I2C bus
 # ads = ADS.ADS1115(i2c)
@@ -25,19 +25,18 @@ for channel in range(8):
  # si addresse ==0x48 :
                     #ads1 = ADS.ADS1115()
 # For each sensor, create it using the TCA9548A channel instead of the I2C object
-        ads1 = ADS.ADS1115(tca[0])
-        ads2 = ADS.ADS1115(tca[1])
+ads1 = ADS.ADS1115(tca[0])
+ads2 = ADS.ADS1115(tca[1])
 
 
 # Create differential input between channel 2 and 3
-        chan0 = AnalogIn(ads1, ADS.P2, ADS.P3)
-# chan1 = AnalogIn(ads1, ADS.P2, ADS.P3)
+chan0 = AnalogIn(ads1, ADS.P2, ADS.P3)
+chan1 = AnalogIn(ads2, ADS.P2, ADS.P3)
 
-        print("{:>5}\t{:>5}".format("raw", "v"))
+print("{:>5}\t{:>5}".format("chan raw", "v"))
 
-        while True:
-            print("{:>5}\t{:>5.3f}".format(chan0.value, chan0.voltage))
-            # print("{:>5}\t{:>5.3f}".format(chan1.value, chan1.voltage))
-            time.sleep(1)
-    
-        tca[channel].unlock()
+while True:
+    print("0","{:>5}\t{:>5.3f}".format(chan0.value, chan0.voltage))
+    print("1","{:>5}\t{:>5.3f}".format(chan1.value, chan1.voltage))
+    time.sleep(1)
+tca[channel].unlock()
