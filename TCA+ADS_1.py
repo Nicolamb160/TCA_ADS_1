6c959c137274b58425b5b2a9263240aad2de64a3
Nicolamb160 <69460211+Nicolamb160@users.noreply.github.com>
2022-03-10 09:56:22 +0100

    Update TCA+ADS_1.py

--- TCA+ADS_1.py
+++ TCA+ADS_1.py
@@ -10,13 +10,20 @@ import adafruit_tca9548a
 i2c = board.I2C()  # uses board.SCL and board.SDA
 
 # Create the TCA9548A object and give it the I2C bus
-tca = adafruit_tca9548a.TCA9548A(i2c, address=0x70)
+tca = adafruit_tca9548a.TCA9548A(i2c, address=0X70)
+
+for channel in range(8):
+    if tca[channel].try_lock():
+        print("Channel {}:".format(channel), end="")
+        addresses = tca[channel].scan()
+        print([hex(address) for address in addresses if address != 0x70])
 
 # Create the ADC object using the I2C bus
 # ads = ADS.ADS1115(i2c)
 # you can specify an I2C adress instead of the default 0x48
 # ads1 = ADS.ADS1115(i2c, address=0x48)
-
+ # si addresse ==0x48 :
+                    #ads1 = ADS.ADS1115()
 # For each sensor, create it using the TCA9548A channel instead of the I2C object
 ads1 = ADS.ADS1115(tca[0])
 ads2 = ADS.ADS1115(tca[1])
